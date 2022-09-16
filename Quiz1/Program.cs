75afdbc376692addbaad0fb9fb8debba5237dfa7
Supatham-1064 <111520592+Supatham-1064@users.noreply.github.com>
2022-09-16 16:13:33 +0700

    Add files via upload

---
+++ Quiz1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class programluk{
+    public static void Main(string[] args) {
+        int Choss;
+        Console.WriteLine("**********------------------------**********");
+        Console.WriteLine("*/           Choose your menu             /*");
+        Console.WriteLine("*/1.Basic information of each university  /*");
+        Console.WriteLine("*/2.University representative information /*");
+        Console.WriteLine("*/3.Information on the electricity        /*");
+        Choss = int.Parse(Console.ReadLine());
+        switch(Choss)
+        {
+            case 1 : datatable.Printbasicmoo();
+                break;
+            case 2 : datatable.PrintTanmoo();
+                break;
+            case 3 : datatable.PrintElbill();
+                break;    
+            default: Console.WriteLine("*******Error Try again*******");
+                break;
+        }
+
+
+    }
+
+}
---
+++ Quiz1/usermoo.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class datatable {
+    public static void Printbasicmoo(){
+        Console.WriteLine("University    :/library name:/Address            :/ Contact number ");
+        Console.WriteLine("Chulalongkorn :/Rak dee     :/Pathumwan Bangkok  :/ 02-215-3555 ");
+        Console.WriteLine("Mahidol       :/Rak lok     :/SalayaNakhon Pathom:/ 02-849-6211 ");
+        Console.WriteLine("Lat Krabang   :/Gay ki      :/Lat Krabang Bangkok:/ 02-329-8000 ");
+        Console.WriteLine("Khon Kaen     :/Wattana     :/Ni meng Khon Kaen  :/ 043-002539 ");
+        Console.WriteLine("Kmutt         :/Athon       :/Thung KhruBangkok  :/ 02-470-8000 ");
+
+    }
+    public static void PrintTanmoo(){
+        Console.WriteLine("University    :/Name    :/Surname      :/ ID card code ");
+        Console.WriteLine("Chulalongkorn :/Chaidee :/Rak dee      :/ 153-698-4561237 ");
+        Console.WriteLine("Mahidol       :/Nongdee :/Ja dee       :/ 742-112-5687412 ");
+        Console.WriteLine("Lat Krabang   :/Aladee  :/Rian dee     :/ 541-772-3215689 ");
+        Console.WriteLine("Khon Kaen     :/Tumdee  :/Work dee     :/ 777-772-4578923 ");
+        Console.WriteLine("Kmutt         :/Nongdee :/Rian dee     :/ 003-852-4752000 ");
+
+    }
+    public static void PrintElbill(){
+        Console.WriteLine("University    :/library name :/Total FT  :/Electricity bill ");
+        Console.WriteLine("Khon Kaen     :/Wattana      :/30.2  FT  :/ 4369  Bath  ");
+        Console.WriteLine("Mahidol       :/Rak lok      :/70.7  FT  :/ 7725  Bath ");
+        Console.WriteLine("Chulalongkorn :/Rak dee      :/79.6  FT  :/ 7911  Bath ");
+        Console.WriteLine("Kmutt         :/Athon        :/208.9 FT  :/ 21470 Bath "); 
+        Console.WriteLine("Lat Krabang   :/Gay ki       :/249   FT  :/ 25172 Bath ");
+        
+    }
+        
+}
